2e2f477c7b084bdcaac5f03bd2f3974e099720bd
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-02 17:49:28 +0700

    add Operator

---
+++ basic programming/operasi.js
@@ -0,0 +1,21 @@
+let myangka = 10 + 10;
+console.log(myangka);
+
+let myname = "novi" + " sriramadhani";
+console.log(myname);
+
+let firstname = "novita sri";
+let lastname = "ramadhani";
+let fulname = firstname + " " + lastname;
+console.log(fulname);
+
+let angka4 = 7;
+let angka5 = 4;
+let perkalian = angka4 * angka5;
+console.log(perkalian);
+
+let pembagian = angka4 / angka5;
+let pertambahan = angka4 + angka5;
+let sisabagi = angka4 % angka5;
+let pengurangan = angka4 - angka5;
+console.log(sisabagi);
---
+++ basic programming/variabel.js
@@ -0,0 +1,17 @@
+var myname = "novi";
+let myage = 19;
+const myAdress = "Stabat";
+
+console.log(myname);
+console.log(myage);
+console.log(myAdress);
+
+let isMahasiswa = true;
+console.log(isMahasiswa);
+
+let myquen;
+
+console.log(myquen);
+
+let myminum = null;
+console.log(myminum);
