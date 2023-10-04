65e1b496880e49493854ea89714dff33fe5517a3
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-04 15:14:51 +0700

    add perbandingan

---
+++ basic programming/operasi/incerement.js
@@ -0,0 +1,19 @@
+var z = 20;
+z * 3;
+console.log(z);
+
+// ini perkalian 3
+z *= 3;
+// ini pertambahan 3
+z += 3;
+// ini perkurangan 3
+z -= 3;
+// ini pembagian 3
+z /= 3;
+// ini sisabagi 3
+z %= 3;
+
+console.log(z);
+
+boolean = true + true;
+console.logboolean;
---
+++ basic programming/operasi/perbandingan.js
@@ -0,0 +1,13 @@
+// ini perbandingan lebih besar
+let perbandingan = 5 > 4;
+// ini perbandingan lebih kecil
+let samadengan = 8 < 4;
+// ini perbandingan tidak sama dengan
+let tidaksamadengan = 10 !== 7;
+// ini perbandingan lebih dari atau sama dengan
+let lebihsatu = 12 >= 8;
+// ini perbandingan kurang dari satu
+let kurangdarisatu = 10 <= 5;
+// ini perbandingan ternary operator
+
+console.log(perbandingan);
