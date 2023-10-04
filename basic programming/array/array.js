8b787d216e1f183bfaece1f89ac4773814100c92
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-04 17:11:18 +0700

    add array

---
+++ basic programming/array/array.js
@@ -0,0 +1,7 @@
+// ini array
+let wacana = ["novi", "inne", "rindi", "meng"];
+console.log(wacana[0]);
+
+let Number = [1, 2, 3, 4, 5];
+let penjumlahan = Number[4] + Number[1];
+console.log(penjumlahan);
