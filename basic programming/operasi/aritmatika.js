4346bad9115469cf2117934ebacb2ca92f4b6a34
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-04 14:31:11 +0700

    add operasi aritmatika

---
+++ basic programming/operasi/aritmatika.js
@@ -0,0 +1,13 @@
+var x = 10;
+var y = 5;
+let penambahan = x + y;
+let perkurangan = x - y;
+let perkalian = x * y;
+let pembagian = x / y;
+let sisabagi = x % y;
+
+console.log("x + y : ", penambahan);
+console.log("x - y : ", perkurangan);
+console.log("x * y : ", perkalian);
+console.log("x / y : ", pembagian);
+console.log("x % y : ", sisabagi);
