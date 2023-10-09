96e4abc08a7d107ca531e6e9d9293ea7949643b5
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-09 16:41:25 +0700

    add perkondisian

---
+++ basic programming/perkondisian/perkondisian.js
@@ -0,0 +1,26 @@
+let mahasiswa = true;
+if (mahasiswa == true) {
+  console.log("aku mahasiswa");
+} else {
+  console.log("aku bukan mahasiswa");
+}
+
+let nilai = 1;
+let isMahasiswa = true;
+if ("isMahasiswa ++ true") {
+  nilai++;
+} else {
+}
+nilai--;
+console.log(nilai);
+
+let angka = 75;
+let hasil = "nilaiku";
+if (angka > 90) {
+  hasil = "nilai sangat bagus";
+} else if (angka > 70 && angka < 50) {
+  hasil = "nilai cukup bagus";
+} else if (angka < 50) {
+  hasil = "nilai bagus";
+}
+console.log(hasil);
