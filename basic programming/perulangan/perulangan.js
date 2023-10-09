069c9bb33302ff2623308b7dde17b2674fea3555
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-09 17:37:03 +0700

    add perulangan

---
+++ basic programming/perulangan/perulangan.js
@@ -0,0 +1,10 @@
+for (let index = 1; index <= 100; index += 2) {
+  console.log(index);
+}
+
+let fruits = ["mangga", "rambutan", "jambu"];
+for (let index = 1; index < 3; index++) console.log(fruits[index]);
+
+for (let lastindex = 0; index < fruits.length; index++) {
+  console.log(fruits[index]);
+}
